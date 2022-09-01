7711ee424f9455ad5eb08f029973fb2a934bd1c2
Kirill Zolotarev <49363725+telikey@users.noreply.github.com>
2022-09-01 12:49:15 +0300

    Init

---
+++ NeuralEngine/NeuralEngine/NeuralEngine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace NeuralEngine
+{
+    public interface IModelInput
+    {
+
+        [ColumnName("Class"), LoadColumn(2)]
+        public bool Class { get; set; }
+    }
+
+    public interface IModelOutput
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Score { get; set; }
+    }
+
+    public class NeuralEngine<TInput, TOutput> where TInput : class, new() where TOutput : class, new()
+    {
+        MLContext mlContext { get; set; }
+
+        dynamic PipeLine { get; set; }
+
+        ITransformer? Model { get; set; }
+
+        public NeuralEngine(int numberOfIterations = 10000, double learningRate = 0.001)
+        {
+            mlContext = new MLContext();
+
+            var props = typeof(TInput).GetProperties().Where(x => x.Name != "Class").Select(x => x.Name).ToArray();
+
+            var dataProcessPipeline = mlContext.Transforms.Concatenate("Features", props);
+            var trainer = mlContext.BinaryClassification.Trainers.SgdCalibrated(labelColumnName: "Class", featureColumnName: "Features", numberOfIterations: numberOfIterations, learningRate: learningRate);
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+            PipeLine = trainingPipeline;
+        }
+
+        public void Train(TInput[] trainInput)
+        {
+            IDataView trainingData = mlContext.Data.LoadFromEnumerable(trainInput);
+            Model = PipeLine.Fit(trainingData);
+        }
+
+        public TOutput? Think(TInput input)
+        {
+            if (Model != null)
+            {
+                var predEngine = mlContext.Model.CreatePredictionEngine<TInput, TOutput>(Model);
+
+                var answ = predEngine.Predict(input);
+                return answ;
+            }
+            return null;
+        }
+    }
+}
---
+++ NeuralEngine/NeuralEngineTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using NeuralEngine;
+
+namespace NeuralEngineTest
+{
+    public class Programm
+    {
+        public class XORModelInput : IModelInput
+        {
+            public float x1 { get; set; }
+            public float x2 { get; set; }
+            public bool Class { get; set; }
+        }
+        public class XORModelOutput : IModelOutput
+        {
+            public bool Prediction { get; set; }
+            public float Score { get; set; }
+        }
+        public static void Main()
+        {
+            var neu = new NeuralEngine<XORModelInput, XORModelOutput>();
+            neu.Train(new XORModelInput[]
+            {
+                new XORModelInput(){ x1=0,x2=0,Class=false},
+                new XORModelInput(){ x1=1,x2=0,Class=true},
+                new XORModelInput(){ x1=0,x2=1,Class=true},
+                new XORModelInput(){ x1=1,x2=1,Class=false},
+            });
+
+            var answ = neu.Think(new XORModelInput() { x1 = 0, x2 = 0 });
+            answ = neu.Think(new XORModelInput() { x1 = 0, x2 = 1 });
+            answ = neu.Think(new XORModelInput() { x1 = 1, x2 = 0 });
+            answ = neu.Think(new XORModelInput() { x1 = 1, x2 = 1 });
+
+
+            Console.ReadKey();
+        }
+    }
+}
